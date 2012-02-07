c7583ee4e3bce2e5f21780900a025687bcdea805
Nan Liu <nan@puppetlabs.com>
2012-02-07 14:01:28 -0800

    setup vcsrepo and hiera-puppet module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,13 +15,31 @@ class appdirector (
     provider => $provider,
   }
 
-  $puppet_confdir = inline_template("<%= Puppet[:confdir] %>")
+  # not dependable:
+  $confdir    = inline_template("<%= Puppet[:confdir] %>")
+  $modulepath = inline_template("<%= Puppet[:modulepath] %>")
 
-  file { "${puppet_confdir}/hiera.yaml":
+  file { "${confdir}/hiera.yaml":
     owner  => 'puppet',
     group  => 'puppet',
     mode   => '0644',
     source => 'puppet:///modules/appdirector/hiera.yaml',
+  }
+
+  Exec {
+    path => '/usr/local/bin:/usr/bin:/bin',
+  }
+
+  exec { 'vcsrepo':
+    command => 'git clone git://github.com/puppetlabs/puppet-vcsrepo vcsrepo',
+    cwd     => $modulepath,
+    creates => "${modulepath}/vcsrepo",
+  }
+
+  exec { 'hiera-puppet':
+    command => 'git clone git://github.com/puppetlabs/hiera-puppet',
+    cwd     => $modulepath,
+    creates => "${modulepath}/hiera-puppet",
   }
 
 }
